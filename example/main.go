c74ade435bacac9557086f3d2f2379bcc09115d4
Michael Kuehl <mickuehl@ratchet.cc>
2020-07-02 21:30:29 +0200

    implemented a singleton pattern for svc

--- example/main.go
+++ example/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/gin-gonic/gin"
 	"github.com/txsvc/platform/pkg/platform"
 	"github.com/txsvc/service/pkg/svc"
 )
@@ -26,13 +28,22 @@ func main() {
 	}()
 
 	// create the service endpoint
-	api, err := svc.New()
+	err := svc.New()
 	if err != nil {
 		os.Exit(1)
 	}
 	// add basic routes
-	api.AddDefaultEndpoints()
-	api.ServeStaticAssets("/", "./example/public")
+	svc.AddDefaultEndpoints()
+	svc.ServeStaticAssets("/", "./example/public")
+	// custom endpoints
+	svc.GET("/api/public", TestAPIResponse)
+	svc.GET("/api/private", TestAPIResponse)
+
 	// add the router to a server on $PORT
-	api.Start()
+	svc.Start()
+}
+
+// TestAPIResponse is the default way to respond to API requests
+func TestAPIResponse(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"status": "ok"})
 }
--- pkg/svc/service.go
+++ pkg/svc/service.go
@@ -2,6 +2,7 @@ package svc
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 
@@ -17,60 +18,90 @@ type (
 	}
 )
 
-var routes map[string]*gin.Engine
+var service *APIService
 
 func init() {
-	// initialize the registry
-	routes = make(map[string]*gin.Engine)
 	// basic http stack config
 	gin.DisableConsoleColor()
+	// make sure the service is not initialized
+	service = nil
 }
 
 // New creates a new service instance on addr
-func New(addr ...string) (*APIService, error) {
+func New(addr ...string) error {
 	localAddr := "0.0.0.0:8080"
 	if addr == nil && len(addr) > 0 {
 		localAddr = addr[0]
 	}
-	if _, ok := routes[localAddr]; ok {
-		return nil, errors.New(fmt.Sprintf("A router for this address already exists: %s", localAddr))
+	if service != nil {
+		return errors.New(fmt.Sprintf("A router for this address already exists: %s", localAddr))
 	}
 
 	// a new router
 	r := gin.New()
 	// Recovery middleware recovers from any panics and writes a 500 if there was one.
 	r.Use(gin.Recovery())
-	// register the router
-	routes[localAddr] = r
 
 	s := &APIService{
 		router: r,
 		addr:   localAddr,
 	}
+	service = s
 
-	return s, nil
+	return nil
 }
 
 // AddDefaultEndpoints adds a couple of simple handlers to the router
-func (api *APIService) AddDefaultEndpoints() {
+func AddDefaultEndpoints() {
 	// default static endpoints
-	api.router.GET("/robots.txt", RobotsEndpoint)
-	api.router.GET("/ads.txt", NullEndpoint)    // FIXME change to the real handler
-	api.router.GET("/humans.txt", NullEndpoint) // FIXME change to the real handler
+	service.router.GET("/robots.txt", RobotsEndpoint)
+	service.router.GET("/ads.txt", NullEndpoint)    // FIXME change to the real handler
+	service.router.GET("/humans.txt", NullEndpoint) // FIXME change to the real handler
 }
 
 // ServeStaticAssets adds handlers to serve static assets
-func (api *APIService) ServeStaticAssets(path, dir string) {
+func ServeStaticAssets(path, dir string) {
 	// routes to load static assets and templates
-	api.router.Use(static.Serve(path, static.LocalFile(dir, true)))
+	service.router.Use(static.Serve(path, static.LocalFile(dir, true)))
 }
 
-// Start attaches the router to a server
-func (api *APIService) Start() {
-	api.router.Run(api.addr)
+// Start attaches the router to a server. This function blocks!
+func Start() {
+	service.router.Run(service.addr)
 }
 
-// Router gives access to the router instance
-func (api *APIService) Router() *gin.Engine {
-	return api.router
+// GET is a shortcut for router.Handle("GET", path, handle).
+func GET(relativePath string, handler gin.HandlerFunc) gin.IRoutes {
+	return service.router.Handle(http.MethodGet, relativePath, handler)
+	//return service.router.Handle(http.MethodGet, relativePath, handlers)
+}
+
+// POST is a shortcut for router.Handle("POST", path, handle).
+func POST(relativePath string, handler gin.HandlerFunc) gin.IRoutes {
+	return service.router.Handle(http.MethodPost, relativePath, handler)
+}
+
+// DELETE is a shortcut for router.Handle("DELETE", path, handle).
+func DELETE(relativePath string, handler gin.HandlerFunc) gin.IRoutes {
+	return service.router.Handle(http.MethodDelete, relativePath, handler)
+}
+
+// PATCH is a shortcut for router.Handle("PATCH", path, handle).
+func PATCH(relativePath string, handler gin.HandlerFunc) gin.IRoutes {
+	return service.router.Handle(http.MethodPatch, relativePath, handler)
+}
+
+// PUT is a shortcut for router.Handle("PUT", path, handle).
+func PUT(relativePath string, handler gin.HandlerFunc) gin.IRoutes {
+	return service.router.Handle(http.MethodPut, relativePath, handler)
+}
+
+// OPTIONS is a shortcut for router.Handle("OPTIONS", path, handle).
+func OPTIONS(relativePath string, handler gin.HandlerFunc) gin.IRoutes {
+	return service.router.Handle(http.MethodOptions, relativePath, handler)
+}
+
+// HEAD is a shortcut for router.Handle("HEAD", path, handle).
+func HEAD(relativePath string, handler gin.HandlerFunc) gin.IRoutes {
+	return service.router.Handle(http.MethodHead, relativePath, handler)
 }
